b9bf22603e1c2ca00dda1d90189c4cf811495c32
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-18 09:46:26 -0300

    Conhencendo a linguagem. iniciando com tabelas

---
+++ lua_inicindo/game.lua
@@ -0,0 +1,16 @@
+
+idades = { 20, 42, 54, 57, 11, 30, 18 }
+
+for i, idades in ipairs(idades) do 
+    print(i, idades)
+end
+
+print(" -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ")
+
+
+fruteira = {"banana", "pera", "melancia", "uva", "abacate", "morango", "maçã"}
+
+for i,fruteira in pairs(fruteira) do
+    print(i, fruteira)
+end
+
