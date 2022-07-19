fff8cfa951256a7c868d27027f5567a24207d4cb
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-07-18 22:11:35 -0300

    Incinado a construção do jogo |add background

--- lua_inicindo/main.lua
+++ lua_inicindo/main.lua
@@ -1,23 +1,38 @@
 
 -- Load some default values for our rectangle.
 function love.load()
+    love.window.setMode(320, 480, { resizable = false})
+    love.window.setTitle('SpaceNav')
+
+    background = love.graphics.newImage('img/background.png')
+
     x, y, w, h = 20, 20, 60, 20
 end
 
 -- -- Increase the size of the rectangle every frame.
 function love.update(dt)
-    w = w + 1
-    h = h + 1
+    if love.keyboard.isDown('w') or love.keyboard.isDown('up') then
+        y = y - 1
+    end
+    if love.keyboard.isDown('s') or love.keyboard.isDown('down') then
+        y = y + 1
+    end
+    if love.keyboard.isDown('a') or love.keyboard.isDown('left') then
+        x = x - 1
+    end
+    if love.keyboard.isDown('d') or love.keyboard.isDown('right') then
+        x = x + 1
+    end
 end
 
 -- -- Draw a coloured rectangle.
 function love.draw()
-    -- In versions prior to 11.0, color component values are (0, 102, 102)
-    love.graphics.setColor(0, 0.4, 0.4)
-    love.graphics.rectangle("fill", x, y, w, h)
+
+    love.graphics.draw(background, 0, 0)
+    love.graphics.circle("line", x, y, 15)
 end
 
-function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
-end
 
+-- function love.draw()
+--     love.graphics.print('Hello World!', 400, 300)
+-- end
